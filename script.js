f2d0eee28d4649279d56a7ea6c7e6587ebc6d296
Pa Yaw <117020650+PapsPython@users.noreply.github.com>
2022-12-16 18:32:17 +0000

    The initial input values for bill and number of people set to 0

--- script.js
+++ script.js
@@ -15,12 +15,12 @@ function TipPaymnts(x) {
 
   totalTipAmt = ((x / 100) * billCheque)
 
-  totalTipPerp = totalTipAmt / numPeople
+  totalTipPerp = Math.round(totalTipAmt / numPeople)
 
   TipPerEl.textContent = "$" + "" + totalTipPerp
 
   totalcashCost = totalTipAmt + Number(billCheque)
-  tipPerson = totalcashCost / numPeople
+  tipPerson = Math.round(totalcashCost / numPeople)
   tipEl.textContent = "$" + "" + tipPerson
   return tipPerson
 }
@@ -52,8 +52,16 @@ function fifty_percent() {
   TipPaymnts(50)
 }
 
+function custom_percent() {
+  custom_percent_value = document.getElementById("custom_percent").value;
+  TipPaymnts(custom_percent_value);
+} 
+
 function reset() {
   tipEl.textContent = "$" + "" + 0.00
   TipPerEl.textContent = "$" + "" + 0.00
+  document.getElementById("billAmt").value = 0
+
+  document.getElementById("numPeople").value = 0
 }
 
